5fe5d85b1d35d3ee05225a5dc73b9ab4d9aea655
Kene_Okeke <kenekc18@gmail.com>
2022-08-26 20:59:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let Thankyou_card = document.getElementById("box");
+let Rating_card = document.getElementById("boxx");
+
+function Open_box(){
+    Thankyou_card.classList.add("open_box");
+    Rating_card.classList.add("close_box")
+}
